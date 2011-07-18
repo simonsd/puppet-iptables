65726cc74c755ef33feee0f6df5530ef30f83a76
Dave Simons <dave@inuits.be>
2011-07-18 10:14:05 +0200

    add config.pp add resource chain in encompassing class fix syntax (no .pp on import
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,12 @@
 import 'packages.pp'
 import 'service.pp'
 import 'cobbler.pp'
+import 'config.pp'
 
 class iptables {
-	include 'iptables::packages.pp'
-	include 'iptables::service.pp'
+	include 'iptables::packages'
+	include 'iptables::service'
+	include 'iptables::config'
+
+	Class['iptables::packages'] -> Class['iptables::config'] -> Class['iptables::service']
 }
