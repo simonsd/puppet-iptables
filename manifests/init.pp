85aba12cedcf522b0ffe785b1f5f74458634ece1
Dave Simons <dave@inuits.be>
2011-11-24 17:18:39 +0100

    remove stages support
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,29 +8,17 @@ class iptables (
 	$running = 'running',
 	$startup = 'true',
 	$status = 'present',
-	$stages = 'no',
 	$savefile = $::operatingsystem ? {
 		default => '/etc/sysconfig/iptables',
 		archlinux => '/etc/iptables/iptables.rules',
 	}
-) {
-	if $stages != 'yes' {
-		class{
-			'iptables::packages':
-				before => Class['iptables::config'];
-			'iptables::config':
-				before => Class['iptables::service'];
-			'iptables::service':;
-		}
-	} else {
-		class {
-			'iptables::packages':
-				stage => depends;
-			'iptables::config':
-				stage => config;
-			'iptables::service':
-				stage => services;
-		}
+){
+	class {
+		'iptables::packages':
+			before => Class['iptables::config'];
+		'iptables::config':
+			before => Class['iptables::service'];
+		'iptables::service':;
 	}
 
 	if $::operatingsystem == archlinux {
